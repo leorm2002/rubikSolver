0e494254cefd59d61fe1ed57fceec24fbb35c831
Leonardo Naddei <naddeileonardo@gmail.com>
2020-06-01 18:43:09 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,15 +1,21 @@
-#include <Stepper.h>
+
 #include <Servo.h>
 
-int arraySize;
-String letters = "";
-Stepper cubo(2048, 11, 9, 10, 8);
+const int pinDirezione = 5;
+const int pinStep = 6;
+
+const int numeroStepMotore = 200; //E' il numero di step per rotazione del motore (potrebbe differire in base al modello)
+const long velocita = 1500; //Si tratta di microsecondi tra un impulso e l'altro sul pin STEP
+
 Servo braccio;
 void setup() {
+  pinMode(pinStep, OUTPUT);
+  pinMode(pinDirezione, OUTPUT);
+  braccio.attach(9);
   Serial.begin(9600);
   Serial.println("Ready");
-  cubo.setSpeed(10);
-  braccio.attach(12);
+  digitalWrite(pinDirezione, LOW);
+
 }
 
 void loop() {
@@ -26,21 +32,21 @@ void loop() {
 void switchLetter(String msg) {
   if (msg == "top") {
     Serial.print("done");
-     while (Serial.readString() != "done") {
+    while (Serial.readString() != "done") {
       ;
     }
-        Serial.println("ready");
+    Serial.println("ready");
 
   }
   else if (msg == "front") {
-    giraCubo();
+    ribaltaCubo();
     Serial.print("done");
     while (Serial.readString() != "done") {
       ;
     }
-    giraCubo();
-    giraCubo();
-    giraCubo();
+    ribaltaCubo();
+    ribaltaCubo();
+    ribaltaCubo();
     Serial.println("ready");
 
 
@@ -48,50 +54,50 @@ void switchLetter(String msg) {
   else if (msg == "left") {
     cuboLibero();
     ruotaFondo(-90);
-    giraCubo();
+    ribaltaCubo();
     Serial.println("done");
     while (Serial.readString() != "done") {
       ;
     }
-    giraCubo();
+    ribaltaCubo();
 
     ruotaFondo(90);
     Serial.println("ready");
-    }
-    else if (msg == "right") {
+  }
+  else if (msg == "right") {
     cuboLibero();
     ruotaFondo(90);
-    giraCubo();
+    ribaltaCubo();
     Serial.println("done");
     while (Serial.readString() != "done") {
       ;
     }
-    giraCubo();
+    ribaltaCubo();
     ruotaFondo(-90);
     Serial.println("ready");
 
   }
   else if (msg == "bottom") {
-    giraCubo();
-    giraCubo();
+    ribaltaCubo();
+    ribaltaCubo();
     Serial.println("done");
     while (Serial.readString() != "done") {
       ;
     }
-    giraCubo();
-    giraCubo();
+    ribaltaCubo();
+    ribaltaCubo();
     Serial.println("ready");
 
   }
   else if (msg == "back") {
     cuboLibero();
     ruotaFondo(180);
-    giraCubo();
+    ribaltaCubo();
     Serial.println("done");
     while (Serial.readString() != "done") {
       ;
     }
-    giraCubo();
+    ribaltaCubo();
     Serial.println("ready");
 
   }
@@ -102,33 +108,33 @@ void switchLetter(String msg) {
   }
   else if (msg == "T") {
     cuboLibero();
-    giraCubo();
-    giraCubo();
+    ribaltaCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(90);
-    giraCubo();
-    giraCubo();
+    ribaltaCubo();
+    ribaltaCubo();
     Serial.println("ready");
   }
   else if (msg == "F") {
     cuboLibero();
     ruotaFondo(180);
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(90);
     cuboLibero();
-    giraCubo();
+    ribaltaCubo();
 
     Serial.println("ready");
   }
   else if (msg == "R") {
     cuboLibero();
     ruotaFondo(-90);
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(90);
 
-    giraCubo();
+    ribaltaCubo();
     ruotaFondo(90);
 
     Serial.println("ready");
@@ -136,20 +142,20 @@ void switchLetter(String msg) {
   else if (msg == "L") {
     cuboLibero();
     ruotaFondo(90);
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(90);
 
-    giraCubo();
+    ribaltaCubo();
     ruotaFondo(-90);
     Serial.println("ready");
   }
   else if (msg == "B") {
     cuboLibero();
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(90);
-    giraCubo();
+    ribaltaCubo();
     ruotaFondo(180);
     Serial.println("ready");
   }
@@ -160,33 +166,33 @@ void switchLetter(String msg) {
   }
   else if (msg == "T'") {
     cuboLibero();
-    giraCubo();
-    giraCubo();
+    ribaltaCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(-90);
-    giraCubo();
-    giraCubo();
+    ribaltaCubo();
+    ribaltaCubo();
     Serial.println("ready");
   }
   else if (msg == "F'") {
     cuboLibero();
     ruotaFondo(180);
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(-90);
     cuboLibero();
-    giraCubo();
+    ribaltaCubo();
 
     Serial.println("ready");
   }
   else if (msg == "R'") {
     cuboLibero();
     ruotaFondo(-90);
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(-90);
 
-    giraCubo();
+    ribaltaCubo();
     ruotaFondo(90);
 
     Serial.println("ready");
@@ -194,20 +200,20 @@ void switchLetter(String msg) {
   else if (msg == "L'") {
     cuboLibero();
     ruotaFondo(90);
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(-90);
 
-    giraCubo();
+    ribaltaCubo();
     ruotaFondo(-90);
     Serial.println("ready");
   }
   else if (msg == "B'") {
     cuboLibero();
-    giraCubo();
+    ribaltaCubo();
     bloccaCubo();
     ruotaFondo(-90);
-    giraCubo();
+    ribaltaCubo();
     ruotaFondo(180);
     Serial.println("ready");
 
@@ -218,25 +224,48 @@ void switchLetter(String msg) {
 
 
 void bloccaCubo() {
-  braccio.write(50);
+  braccio.write(130);
   Serial.println("Blocco cubo");
+  delay(400);
 }
 
 void cuboLibero() {
-  braccio.write(30);
+  braccio.write(80);
   Serial.println("Cubo libero");
-
+  delay(400);
 }
-void giraCubo() {
-  braccio.write(110);
-  delay(100);
-  braccio.write(30);
+void ribaltaCubo() {
+  braccio.write(80);
+  delay(500);
+  int pos;
+  for (pos = 80; pos <= 165; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    braccio.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(7);                       // waits 15ms for the servo to reach the position
+  }
+  braccio.write(80);
+  delay(1000);
   Serial.println("Cubo girato");
 
 }
 
-void ruotaFondo(int gradi ) {
-  cubo.step(map(gradi, 0, 360, 0, 2048));
-  delay(1000);
-  Serial.println("Cubo ruotato di: " + String(gradi) + " gradi");
+void ruotaFondo(int deg ) {
+  if (deg > 0) {
+    for (int x = 0; x < map(deg, 0, 360, 0, 200); x++) {
+      digitalWrite(pinStep, HIGH);
+      delayMicroseconds(velocita);
+      digitalWrite(pinStep, LOW);
+      delayMicroseconds(velocita);
+    }
+  }
+  else {
+    for (int x = map(deg, 0, 360, 0, 200); x > 0; x--) {
+      digitalWrite(pinStep, HIGH);
+      delayMicroseconds(velocita);
+      digitalWrite(pinStep, LOW);
+      delayMicroseconds(velocita);
+    }
+  }
+  Serial.println("Cubo ruotato di: " + String(deg) + " gradi");
+  delay(200);
 }
